db0311073359de1597d73088e024d11d95121dbb
sanjitrao16 <sanjitrao1605@gmail.com>
2024-12-25 15:18:03 +0530

    changed font sizes in featured and contact section

--- frontend/src/components/Home Page/About.jsx
+++ frontend/src/components/Home Page/About.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div className="font-semibold text-center text-white text-5xl w-3/4 mx-auto my-10 mb-60 bg-slate-50 bg-opacity-10 backdrop-filter backdrop-blur-xl rounded-lg p-6">
+    <div className="font-semibold text-center text-white text-4xl w-3/4 mx-auto my-10 mb-60 bg-slate-50 bg-opacity-10 backdrop-filter backdrop-blur-xl rounded-lg p-6">
       About
       <div className="font-normal text-justify text-lg mt-6">
-        Tantrotrotsav is the annual technical and non-technical festival of the Amrita Vishwa Vidyapeetham, Chennai Campus, started in 2023. 
-        The festival celebrates the spirit of innovation, engineering, and technology with a diverse range of competitions, workshops, lectures, and exhibitions. 
-        The name "Tantrotrotsav" combines the Sanskrit words "Tantra," meaning technology, and "Utsav," meaning festival, symbolizing a vibrant celebration of technical prowess and creativity. 
-        This fest features an array of over 125 events, including hackathons, coding challenges, robotics competitions, tech-talks by eminent personalities, panel discussions, and hands-on workshops aimed at fostering technical skills and knowledge. 
-        Exhibitions showcasing projects and demonstrations of emerging technologies are also key highlights of the event. 
-        Tantrotrotsav is entirely organized and managed by the students of Amrita Chennai Campus, emphasizing teamwork, leadership, and innovation. It attracts participants from all over India, making it a hub for collaboration, networking, and the exchange of ideas. 
-        Having grown significantly since its inception, Tantrotrotsav continues to inspire and empower young minds to think beyond boundaries and shape the future of technology.{" "}
+        Tantrotrotsav is the annual technical and non-technical festival of the
+        Amrita Vishwa Vidyapeetham, Chennai Campus, started in 2023. The
+        festival celebrates the spirit of innovation, engineering, and
+        technology with a diverse range of competitions, workshops, lectures,
+        and exhibitions. The name "Tantrotrotsav" combines the Sanskrit words
+        "Tantra," meaning technology, and "Utsav," meaning festival, symbolizing
+        a vibrant celebration of technical prowess and creativity. This fest
+        features an array of over 125 events, including hackathons, coding
+        challenges, robotics competitions, tech-talks by eminent personalities,
+        panel discussions, and hands-on workshops aimed at fostering technical
+        skills and knowledge. Exhibitions showcasing projects and demonstrations
+        of emerging technologies are also key highlights of the event.
+        Tantrotrotsav is entirely organized and managed by the students of
+        Amrita Chennai Campus, emphasizing teamwork, leadership, and innovation.
+        It attracts participants from all over India, making it a hub for
+        collaboration, networking, and the exchange of ideas. Having grown
+        significantly since its inception, Tantrotrotsav continues to inspire
+        and empower young minds to think beyond boundaries and shape the future
+        of technology.{" "}
       </div>
     </div>
   );
