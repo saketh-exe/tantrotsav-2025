054e80de697035f9630bc4df104f2c34c4991860
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2025-01-02 11:13:17 +0530

    Minor Changes

--- frontend/src/components/Home Page/Pin.jsx
+++ frontend/src/components/Home Page/Pin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import {cn} from "../../lib/utils";
+import { cn } from "../../lib/utils";
 
 
 export const PinContainer = ({
@@ -20,28 +20,31 @@ export const PinContainer = ({
   };
 
   return (
-    (<a
-      className={cn("relative group/pin z-50   ", containerClassName)}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      target="_blank"
-      href={href || "/"}>
-      <div
-        style={{
-          perspective: "1000px",
-          transform: "rotateX(70deg) translateZ(0deg)",
-        }}
-        className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2">
+    (
+      <><a
+        className={cn("relative group/pin z-50   ", containerClassName)}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        target="_blank"
+        href={href || "/"}>
         <div
           style={{
-            transform: transform,
+            perspective: "1000px",
+            transform: "rotateX(70deg) translateZ(0deg)",
           }}
-          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-slate-900 border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden">
-          <div className={cn(" relative z-50 ", className)}>{children}</div>
+          className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2">
+          <div
+            style={{
+              transform: transform,
+            }}
+            className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-slate-900 border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden">
+            <div className={cn(" relative z-50 ", className)}>{children}</div>
+          </div>
         </div>
-      </div>
-      <PinPerspective title={title} href={href} />
-    </a>)
+        <PinPerspective title={title} href={href} />
+      </a>
+      </>
+    )
   );
 };
 
@@ -54,7 +57,7 @@ export const PinPerspective = ({
       className="pointer-events-none  w-84 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
+          <div
             href={href}
             target={"_blank"}
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
@@ -65,7 +68,7 @@ export const PinPerspective = ({
 
             <span
               className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
+          </div>
         </div>
 
         <div
