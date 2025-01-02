3b34fb26f5d4452afe5d1ca01df8bf3cad8b827e
sanjitrao16 <sanjitrao1605@gmail.com>
2025-01-02 10:21:11 +0530

    updated sponsors

--- frontend/src/components/Home Page/Sponsor.jsx
+++ frontend/src/components/Home Page/Sponsor.jsx
@@ -19,7 +19,7 @@ export default function Sponsor() {
         </div>
         <div className="flex justify-center items-center">
           <img
-            src={Sponsor1} // <= IIT MADRAS maybe the second sponsor.
+            src={Sponsor2} // <= IIT MADRAS maybe the second sponsor.
             alt="Sponsor 2"
             className="w-24 sm:w-32 lg:w-40"
           />
