b2c632fb594e263a45aadfc3c02ba6ce1984211d
sanjitrao16 <sanjitrao1605@gmail.com>
2025-01-02 12:25:30 +0530

    updated sponsors

--- frontend/src/components/Home Page/Sponsor.jsx
+++ frontend/src/components/Home Page/Sponsor.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sponsor1 from "../../assets/Decathlon_Logo.png";
-import Sponsor2 from "../../assets/IIT_Madras_Logo.png";
+import Sponsor2 from "../../assets/e-cell_logo.png";
+import Sponsor3 from "../../assets/EDD_Logo.png";
 
 export default function Sponsor() {
   return (
@@ -8,7 +9,7 @@ export default function Sponsor() {
       <h2 className="font-semibold text-4xl sm:text-2xl md:text-3xl lg:text-4xl  text-center mb-3 text-white">
         Partners & Sponsors
       </h2>
-      <div className="w-full h-auto bg-gradient-to-r from-black via-slate-700 to-black flex flex-wrap justify-center items-center gap-4 p-4 sm:gap-6 sm:p-6 lg:gap-8 lg:p-8 my-6">
+      <div className="w-full h-auto bg-gradient-to-r from-black via-slate-600 to-black flex flex-wrap justify-center items-center gap-4 p-4 sm:gap-6 sm:p-6 lg:gap-8 lg:p-8 my-6">
         {/* Sponsors */}
         <div className="flex justify-center items-center">
           <img
@@ -17,27 +18,20 @@ export default function Sponsor() {
             className="w-24 sm:w-32 lg:w-40"
           />
         </div>
-        {/* <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center">
           <img
-            src={Sponsor2} // <= IIT MADRAS maybe the second sponsor.
+            src={Sponsor2}
             alt="Sponsor 2"
-            className="w-24 sm:w-32 lg:w-40"
-          />
-        </div> */}
-        {/* <div className="flex justify-center items-center">
-          <img
-            src={Sponsor1}
-            alt="Sponsor 3"
             className="w-24 sm:w-32 lg:w-40"
           />
         </div>
         <div className="flex justify-center items-center">
           <img
-            src={Sponsor1}
-            alt="Sponsor 4"
+            src={Sponsor3}
+            alt="Sponsor 3"
             className="w-24 sm:w-32 lg:w-40"
           />
-        </div> */}
+        </div>
       </div>
     </div>
   );
